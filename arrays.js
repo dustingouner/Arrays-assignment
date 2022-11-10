7bce3e235793bb0ec00bd54ecc3cb6a76db10f2d
Dustin Gouner <dustingouner20@gmail.com>
2022-11-09 20:14:00 -0700

    Add index open question

--- arrays.js
+++ arrays.js
@@ -11,3 +11,6 @@ console.log(jerseyNumbers.push())
 
 // The shift method removes the first element in the array and returns that the removed element. 
 console.log(hasSurfed.shift())
+
+
+// The first element in an array has an index number of 0.  Instead of starting from 1 you would count 0, 1, 2, 3. 
