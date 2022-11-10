b556e6857397f1d0271e0c517e2b26afee1d11fa
Dustin Gouner <dustingouner20@gmail.com>
2022-11-09 20:10:57 -0700

    Add call to 3 arrays

--- arrays.js
+++ arrays.js
@@ -1,3 +1,13 @@
 var citysVisited = ["Boston", "Atlanta", "New York", "Chicago" ]
 var jerseyNumbers = [15, 20, 22, 10]
 var hasSurfed = [true, true, false, true]
+
+// The pop method will remove the last element from the array and returns that element. 
+// In this case it will return Chicago. 
+console.log(citysVisited.pop())
+
+// The push method will add one or more elements to the end of an array & return length of new array.
+console.log(jerseyNumbers.push())
+
+// The shift method removes the first element in the array and returns that the removed element. 
+console.log(hasSurfed.shift())
